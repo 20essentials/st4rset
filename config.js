6142cd2436c76dd61e8922749467b222c28ee4e7
DominicDev <s0l0estudi4r@gmail.com>
2026-04-03 20:50:50 +0200

    v0.0.1

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'STARSET';
+export const nameSongs = [
+	"DIE FOR YOU", /*1*/
+	"IT HAS BEGUN", /*2*/
+	"CARNIVORE", /*3*/
+	"MY DEMONS", /*4*/
+	"MONSTER", /*5*/
+	"LET IT DIE", /*6*/
+	"DARK ON ME", /*7*/
+	"ANTIGRAVITY", /*8*/
+	"TELESCOPE", /*9*/
+	"DOWN WITH THE FALLEN", /*10*/
+	"WAITING ON THE SKY TO CHANGE", /*11*/
+	"POINT OF NO RETURN", /*12*/
+	"ECHO ", /*13*/
+	"INFECTED", /*14*/
+	"HALO", /*15*/
+	"RICOCHET", /*16*/
+	"UNBECOMING", /*17*/
+	"PERFECT MACHINE", /*18*/
+	"EVERGLOW", /*19*/
+	"LAST TO FALL", /*20*/
+]
